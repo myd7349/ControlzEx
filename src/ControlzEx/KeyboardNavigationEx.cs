377636cfc00b3899f433e30000be3eb22f8b6bec
myd7349 <myd7349@gmail.com>
2025-10-18 23:01:28 +0800

    Reproduce the bug

--- src/ControlzEx/KeyboardNavigationEx.cs
+++ src/ControlzEx/KeyboardNavigationEx.cs
@@ -63,7 +63,7 @@
             System.Diagnostics.Trace.WriteLine($"=============== {guid} Focus {element}");
             // Background、Input will cause problem.
             // Loaded seems ok.
-            element?.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+            element?.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
             {
                 System.Diagnostics.Trace.WriteLine($"=============== {guid} BeginInvoke {element}");
                 var keybHack = Instance;
